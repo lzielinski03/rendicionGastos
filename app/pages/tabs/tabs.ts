d900bdbdb19f1f15a3b8c86bd80a6077527d145c
lzielinski <lzielinski03@gmail.com>
2016-08-24 14:08:04 -0300

    remove about page, and styles in login

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
 import {ContactPage} from '../contact/contact';
 import {RendicionesPage} from '../rendiciones/rendiciones';
 
@@ -12,14 +11,12 @@ export class TabsPage {
   private tab1Root: any;
   private tab2Root: any;
   private tab3Root: any;
-  private tab4Root: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
+    this.tab2Root = RendicionesPage;
     this.tab3Root = ContactPage;
-    this.tab4Root = RendicionesPage;
   }
 }
