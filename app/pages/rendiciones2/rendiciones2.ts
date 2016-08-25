e433e7adaebf0644d0c42838258df4cfdf1f7ac4
lzielinski <lzielinski03@gmail.com>
2016-08-25 17:05:01 -0300

    last changes

--- app/pages/rendiciones2/rendiciones2.ts
+++ app/pages/rendiciones2/rendiciones2.ts
@@ -3,14 +3,20 @@ import {NavController, LoadingController} from 'ionic-angular';
 import {RendicionPage} from '../rendicion/rendicion';
 import {ExtendedHttp} from './../../services/ExtendedHttp';
 import {UserService} from './../../services/UserService';
+import {RechazoPage} from '../rechazo/rechazo';
 
 @Component({
 	templateUrl: 'build/pages/rendiciones2/rendiciones2.html'
 })
 export class RendicionesPage2 {
 
-	processes: any;
 	instances;
+
+	evaluar = {
+		resultado: 'aprobar',
+		comentario: '',
+		motivo: ''
+	}
 
 	constructor(
 		private navCtrl: NavController,
@@ -36,42 +42,56 @@ export class RendicionesPage2 {
 		});
 	}
 
-	formatData(data) {
-		data.map( instance => {
-			instance.data = JSON.parse(instance.data);
-			if (instance.data.length > 1) instance.montoTotal = instance.data.reduce((a, b) => a.monto + b.monto);
-			else instance.montoTotal = instance.data[0].monto;
-			return instance.receptionTime = new Date(instance.receptionTime);
+	goToRechazo(rendicion) {
+		this.navCtrl.push(RechazoPage, {
+			rendicion: rendicion
 		});
-		this.instances = data;
-		//console.log(data);
-		/*
+	}
+
+	evaluate(rendicion) {
+		let load = this.loading.create();
+		load.present();
+		console.log(rendicion.id);
+		this.http.post('/api/instance/' + rendicion.id + '/evaluate', { "evaluar": this.evaluar }).subscribe(
+			res => load.dismiss(),
+			err => console.log(err),
+			()  => this.popToRootFix()
+		);
+	}
+
+	// workarround for ionic bugs in NavController - LoadingController
+	private popToRootFix() {
+		setTimeout(() => this.navCtrl.popToRoot(), 500)
+	}
+
+	formatData(data) {
 		let last = {
 			anio: null,
 			mes: null,
 			dia: null
 		};
 
-		data.map( x => {
-			return x.activities.map( y => {
-				return y.instances.map( z => {
-					if (
-						last.anio == z.recivedDate.anio &&
-						last.mes == z.recivedDate.mes &&
-						last.dia == z.recivedDate.dia ) {
-						z.print = false;
-					} else {
-						z.print = true;
-					}
-					last.anio = z.recivedDate.anio;
-					last.mes = z.recivedDate.mes;
-					last.dia = z.recivedDate.dia;
+		data.map( instance => {
+			instance.data = JSON.parse(instance.data);
+			if (instance.data.length > 1) instance.montoTotal = instance.data.reduce((a, b) => a.monto + b.monto);
+			else instance.montoTotal = instance.data[0].monto;
+			instance.receptionTime = new Date(instance.receptionTime);
+			instance.receptionTime.get
+			if (
+				last.anio == instance.receptionTime.getYear() &&
+				last.mes == instance.receptionTime.getMonth() &&
+				last.dia == instance.receptionTime.getDay() ) {
+				instance.print = false;
+			} else {
+				instance.print = true;
+			}
+			last.anio = instance.receptionTime.getYear();
+			last.mes = instance.receptionTime.getMonth();
+			last.dia = instance.receptionTime.getDay();
 
-					if ((<String>z.recivedDate.min).length == 1) z.recivedDate.min = <String>('0' + z.recivedDate.min);
-					return z;
-				})
-			})
+
+			return 
 		});
-		this.processes = data;*/
+		this.instances = data;
 	}
 }
